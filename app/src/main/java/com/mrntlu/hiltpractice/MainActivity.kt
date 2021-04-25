d9879606eefbd25084523bde4cb5ab3e03a9631f
Burak <97burakfidan97@gmail.com>
2021-04-25 18:46:04 +0300

    Scoping.

--- app/src/main/java/com/mrntlu/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/mrntlu/hiltpractice/MainActivity.kt
@@ -2,37 +2,61 @@ package com.mrntlu.hiltpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    //Field Injection
     @Inject
     lateinit var someClass: SomeClass
+
+    @Inject
+    lateinit var someOtherClass: SomeOtherClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println(someClass.testFunc())
-        println(someClass.otherTestFunc())
+        println(someOtherClass.testFunc())
     }
 }
 
-class SomeClass
-@Inject
-constructor( //Constructor Injection
-        private val someOtherClass: SomeOtherClass
-){
-    fun testFunc() = "This is a test"
+@AndroidEntryPoint
+class TestFragment: Fragment(){
 
-    fun otherTestFunc() = someOtherClass.otherTestFunc()
+    @Inject
+    lateinit var someClass: SomeClass
+
+    @Inject
+    lateinit var someOtherClass: SomeOtherClass
+
+    @Inject
+    lateinit var someAnotherClass: SomeAnotherClass
 }
 
+@Singleton
+class SomeClass
+@Inject
+constructor(){
+    fun testFunc() = "This is a test"
+}
+
+@ActivityScoped
 class SomeOtherClass
 @Inject
 constructor(){
-    fun otherTestFunc() = "This is test but the second one."
+    fun testFunc() = "This is a test for other"
+}
+
+@FragmentScoped
+class SomeAnotherClass
+@Inject
+constructor(){
+    fun testFunc() = "This is a test for another"
 }
