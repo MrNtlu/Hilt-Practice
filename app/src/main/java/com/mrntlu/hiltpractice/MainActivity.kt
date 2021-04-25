54b2e332bb08753fb14ed8350a219dec6a525ed1
Burak <97burakfidan97@gmail.com>
2021-04-25 18:29:23 +0300

    Git init.

---
+++ app/src/main/java/com/mrntlu/hiltpractice/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mrntlu.hiltpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    //Field Injection
+    @Inject
+    lateinit var someClass: SomeClass
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class SomeClass
+@Inject
+constructor(){
+    fun testFunc() = "This is a test"
+}
---
+++ app/src/main/java/com/mrntlu/hiltpractice/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.mrntlu.hiltpractice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application() {
+
+}
